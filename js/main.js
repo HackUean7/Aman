b4518b7d025212b6beb8ff000c46faf3c6343f15
Aman Srivastav <hack.uean7@gmail.com>
2018-03-30 18:11:38 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ particlesJS.load('plex', 'js/data.json', function() {
   console.log('callback - particles.js config loaded');
 });
 var typed = new Typed('.greet', {
-  strings: ["Hello","Bonjour","Hola","Zdravstvuyte","Nǐn hǎo","Salve","Konnichiwa","Guten Tag"," Olá"," Asalaam Alaikum","Namaste","Hello"],
+  strings: ["Hello","Hola"," Olá","Namaste","Hello"],
   	typeSpeed: 60,
     backSpeed: 50,
     backDelay: 5000,
