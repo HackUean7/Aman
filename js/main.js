0a28216638e53bf9843789d422fb734d2ea1ebb1
Aman Srivastav <hack.uean7@gmail.com>
2022-05-29 10:15:48 +0530

    Create main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ particlesJS.load('plex', 'js/data.json', function() {
   console.log('callback - particles.js config loaded');
 });
 var typed = new Typed('.greet', {
-  strings: ["Hello","Hola"," Asalaam Alaikum","Namaste","Hello"],
+  strings: ["Hello","Namaste"," Asalaam Alaikum","Hola","01001000 01100101 01101100 01101100 01101111"],
   	typeSpeed: 60,
     backSpeed: 50,
     backDelay: 5000,
